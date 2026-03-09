be35445735dc40a1f6893424cc78e03cd4b0a98d
biltema-korv <colbjo@edu.umea.se>
2026-03-09 14:58:22 +0100

    im so ggggdg

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,17 @@
+<script>
+    let value=$state(0)
+    const min=0
+    const max=5000 
+</script>
+
 <p>gork jork it</p>
+
+<main>
+    <h1 class="test"> {value} Poäng</h1>
+</main>
+
+<style>
+    .test {
+        text-align: center;
+    }
+</style>
