5a7a9f7f100ba047437c60413f19087655bd95e3
biltema-korv <colbjo@edu.umea.se>
2026-03-13 13:42:13 +0100

    w

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,13 @@
 <script>
-    let value=$state(0)
+    let points=$state(0)
+    let totalpoints=$state(0)
     const min=0
     const max=5000 
 </script>
 
-<p>gork jork it</p>
-
 <main>
-    <h1 class="test"> {value} Poäng</h1>
+    <h1 class="test"> {points} Poäng</h1>
+    <h1 class="test"> Total Poäng: {points}</h1>
 </main>
 
 <style>
